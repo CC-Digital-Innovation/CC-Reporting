b9072d4e99df0d4a6950cdbf3ed15852b043f483
Ben Verley <48893829+Bverley92@users.noreply.github.com>
2024-01-16 14:04:29 -0700

    added capacity modules for various devices

--- src/storage_device.py
+++ src/storage_device.py
@@ -2,9 +2,10 @@ from dataclasses import dataclass
 
 @dataclass
 class StorageDevice:
-    used_storage: int
-    total_storage: int
+    used_storage: float
+    total_storage: float
     unit_of_measurement: str
+    free_storage   : float
 
     def get_utilization_percentage(self, decimal_precision: int) -> float:
         return round(self.used_storage / self.total_storage, decimal_precision)
