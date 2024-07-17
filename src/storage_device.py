9d07451e6d88a7950c00f39a69972d832795898e
Ben Verley <bverley92@gmail.com>
2024-07-17 16:32:32 -0700

    main restore

---
+++ src/storage_device.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class StorageDevice:
+    used_storage: float
+    total_storage: float
+    unit_of_measurement: str
+    free_storage   : float
+
+    def get_utilization_percentage(self, decimal_precision: int) -> float:
+        return round(self.used_storage / self.total_storage, decimal_precision)
