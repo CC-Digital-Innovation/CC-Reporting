881cafac13f9ea618622eb08525dc535b803d6fe
Ben Verley <benjamin.verley@computacenter.com>
2024-11-20 16:05:29 -0500

    minor bug fixes during test, logging updates

--- src/wrapper.py
+++ src/wrapper.py
@@ -14,6 +14,17 @@ from loguru import logger
 import urllib3
 urllib3.disable_warnings()
 
+#logger
+def logger_init():
+    logger.remove()
+    logger.add(sys.stderr, colorize=True, level="DEBUG")
+    logfile = os.path.join(os.getcwd(), 'latest.log')
+    logger.add(logfile, level='DEBUG')
+    logger.info("*****************************")
+    logger.info("*********Report Start********")
+    logger.info("*****************************")
+
+logger_init()
 #Set up globals from .env
 dotenv.load_dotenv(PurePath(__file__).with_name('.env'))
 
@@ -22,16 +33,18 @@ NOCO_DB_HEADER = {
     "xc-token" : os.getenv('NOCO_TOKEN')
 }
 NOCO_CONFIG_TABLE_ID = os.getenv("NOCO_CONFIG_TABLE_ID")
-CUSTOMER = os.getenv("Customer")
+CUSTOMER = os.getenv("CUSTOMER_NAME")
 def noco_config_retrieval(custname):
     search = {
         "where" : f"(Customer,eq,{custname})"
     }
     
     r = requests.get(f"{NOCO_URL}api/v2/tables/{NOCO_CONFIG_TABLE_ID}/records", params=search, headers=NOCO_DB_HEADER)
+    logger.debug(f"Response from NoCo for config: {r}")
     if r.status_code == 200 and 'list' in r.json().keys():
         return r.json()['list'][0]
     else:
+        logger.debug(f"Error getting config, response message{r.text}")
         return "Error retrieving configuration from NoCo"
 
 def noco_get_devices(custname):
@@ -40,6 +53,7 @@ def noco_get_devices(custname):
         for customer in r.json()['list']:
             if customer['title']==custname:
                 base = requests.get(f"{NOCO_URL}api/v2/meta/bases/{customer['id']}/tables", headers=NOCO_DB_HEADER)
+                logger.debug(f"Response from NoCo for Customer table: {base}")
                 if base.status_code == 200 and 'list' in base.json().keys():
                     for table in base.json()['list']:
                         if table['title'] == 'Devices':
@@ -50,24 +64,30 @@ def noco_get_devices(custname):
                                 page = page +1
                                 query= {"page":page}
                                 repeat_request = requests.get(f"{NOCO_URL}api/v2/tables/{table['id']}/records", params =query, headers=NOCO_DB_HEADER)
+                                logger.debug(f"Response from NoCo for devices: {repeat_request}")
                                 if repeat_request.status_code == 200 and 'list' in repeat_request.json().keys():
                                     for device in repeat_request.json()['list']:
                                         devices.append(device)
                                     if r.json()['pageInfo']['isLastPage']:
                                         flag = False
                                 else:
+                                    logger.debug(f"Error getting devices, Noco Response message: {repeat_request.text}")
                                     return {"Error" : "Error retrieving devices"}
                             return devices
+                    logger.debug(f"Customer Devices table not found")
                     return {"Error" : "Customer devices table not found"}
                 else:
+                    logger.debug(f"Error getting Customer table, Noco Response message: {base.text}")
                     return {"Error" : "Error retrieving base tables"}
+        logger.debug(f"Customer table not found")
         return {"Error" : "customer table not found"}
     else:
+        logger.debug(f"Error getting bases, Noco Response message: {r.text}")
         return {"Error" : "Error retrieving customer tables"}
 
 CONFIG = noco_config_retrieval(CUSTOMER)
 if CONFIG:
-    URL = os.getenv('EMAIL_API_URL')
+    URL = CONFIG['Email API URL']
     EMAILAPI_TOKEN = CONFIG['Email API Token']
     SNOW_INSTANCE = CONFIG['Snow Instance']
     SNOW_USERNAME = CONFIG['Snow User']
@@ -75,7 +95,6 @@ if CONFIG:
     CMDB_PATH     = CONFIG['CMDB Path']
 else:
     raise Exception("Configuration retrieval failed")
-
 snow_client = pysnow.Client(instance=SNOW_INSTANCE, user=SNOW_USERNAME, password=SNOW_PASSWORD)
 
 
@@ -127,23 +146,15 @@ def email_report(directory):
     }
 
     r = requests.request("POST", url = URL, headers=header, data = Data, files=uploadFiles)
-    logger.debug(r.json())
+    logger.debug(r)
     
-def logger_init():
-    logger.remove()
-    logger.add(sys.stderr, colorize=True, level="DEBUG")
-    logger.info("*****************************")
-    logger.info("*********Report Start********")
-    logger.info("*****************************")
-
 
 #Step 1: Get list of devices from noco - for now local list
-logger_init()
 devicedata = noco_get_devices(CUSTOMER)
 
 devicelist = []
-for device in devicedata['devices']:
-    if device['snowName']:
+for device in devicedata:
+    if device['Snow Name']:
         #Step 2: get device data from snow - for now local list
         logger.debug(f"Fetching snow data for {device['Snow Name']}")
         snowdevice = query_Device(device['Snow Name'])
